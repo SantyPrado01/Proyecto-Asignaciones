b0db4d493ac1952d73892a9246698884db9439fb
SantyPrado01 <santiago01prado@gmail.com>
2023-08-04 21:21:03 -0300

    Cambios en Funciones, agregado de Icono

--- asignaciones.py
+++ asignaciones.py
@@ -5,56 +5,46 @@ import re
 from datetime import datetime, timedelta
 import tkinter as tk
 from funciones import *
+import locale
+locale.setlocale(locale.LC_TIME, "es_ES.utf8")
 dibujo = None
+
+
 def generar_asignacion():
-    global ingresar_semana_date, nombre_entry, ayudante_entry, contador_imagenes, dibujo
+    global ingresar_semana_date, nombre_entry, ayudante_entry, dibujo, aclaraciones_entry
     try:
-        mensaje_completado.config(text="")
         texto_actividad = opcion_seleccionada.get().lower()
         texto_nombre = nombre_entry.get()
         texto_ayudante = ayudante_entry.get().capitalize()
-
-        if contador_imagenes == 4:
-            # Sumar una semana a la fecha almacenada en ingresar_semana
-            fecha_dada = datetime.strptime(ingresar_semana_date.get(), "%d-%m-%Y")
-            fecha_dada += timedelta(weeks=1)
-            ingresar_semana_date.set(fecha_dada.strftime("%d-%m-%Y"))
-            mensaje_completado.config(text=f'Asignaciones Completadas para la semana: {fecha_dada.strftime("%d-%m-%Y")}')
-            contador_imagenes = 0
-        
-        contador_imagenes += 1
+        aclaraciones = aclaraciones_entry.get()
 
         # Cargar la imagen original en cada iteración
         imagen = Image.open("image/Asignaciones.jpg")
         imagen = imagen.convert("RGB")
         dibujo = ImageDraw.Draw(imagen)
         fuente = ImageFont.truetype("fonts/Calibri.ttf", 26)
-
+        fuente_aclaraciones = ImageFont.truetype("fonts/Calibri.ttf", 18)
         # Agregar Semana a la Imagen 
         dibujo.text((112,190), ingresar_semana_date.get(), fill=(0, 0, 0), font=fuente)
-       
-        # Reemplazar caracteres no válidos en el nombre de archivo
-        nombre_archivo = re.sub(r"[^\w\s-]", "", texto_nombre.strip())
-        nombre_archivo = re.sub(r"[\s]+", "_", nombre_archivo)
-
-        # Generar el nombre de archivo único basado en el texto
-        nombre_archivo = nombre_archivo + ".jpg"
 
         #Definimos Actividad
-        asignacion_actividad(texto_actividad,dibujo, texto_nombre, texto_ayudante, fuente)
+        asignacion_actividad(texto_actividad,dibujo, texto_nombre, texto_ayudante, fuente, aclaraciones, fuente_aclaraciones)
+
         #Definimos Sala
         asignacion_sala(opcion_principal,opcion_auxiliar,dibujo)
+
         #Guardamos Imagen
-        guardar_imagen(imagen, nombre_archivo, texto_nombre)
+        guardar_imagen(imagen, texto_nombre)
         
         #Mensaje de Asignacion descargada
-        mensaje_completado_nombre.config(text=guardar_imagen(imagen, nombre_archivo, texto_nombre))
+        mensaje_completado_nombre.config(text=guardar_imagen(imagen, texto_nombre))
 
         #Variables Vacias 
         nombre_entry.set("")
         ayudante_entry.set("")
+        aclaraciones_entry.set("")
         mensaje_error.config(text="")
-        
+      
     except Exception as e:
         mensaje_error.config(text=f"Ocurrió un error al generar la asignación {e}")
 
@@ -66,56 +56,47 @@ def activar_generar_asignacion(event):
 
 # Crear la ventana principal
 ventana = Tk()
-
 ventana.title("Generador de Asignaciones")
 ventana.geometry('400x500')
 fuente_opciones = ('Calibri', 22)
 fuentes_seleccionables = ('Calibri', 18)
+icon_path = "image/logo.ico"  
+ventana.iconbitmap(icon_path)
 
 # Variables para almacenar los datos ingresados por el usuario
 ingresar_semana_date = StringVar()
 nombre_entry = StringVar()
 ayudante_entry = StringVar()
-contador_imagenes = 0
-
-#Opciones Asignaciones
-opciones_asignaciones = ['Seleccionar Asignacion', 'Lectura','Primera Conversacion', 'Revisita', 'Curso Biblico', 'Discurso', 'Otro']
 opcion_seleccionada = StringVar()
-opcion_seleccionada.set(opciones_asignaciones[0])
-
-
-#Mensaje Semana Completada
-mensaje_completado = Label(ventana, text="", fg="green")
-mensaje_completado.pack()
+aclaraciones_entry = StringVar()
 
 # Entrada para la fecha
-Label(ventana, text="Seleccionar fecha:", font=fuente_opciones).pack()
-DateEntry(ventana, textvariable=ingresar_semana_date, date_pattern='dd-mm-yyyy', font=fuentes_seleccionables).pack()
-Label(ventana, text='').pack()
+Label(ventana, text='Seleccionar fecha:', font=fuente_opciones).pack()
+DateEntry(ventana, textvariable=ingresar_semana_date, date_pattern='dd-mm-yyyy', font=fuentes_seleccionables, locale="es_ES").pack()
 
 #Entrada para la actividad
 Label(ventana, text='Ingrese la Actividad: ',font=fuente_opciones).pack()
+opciones_asignaciones = ['Seleccionar Asignacion', 'Lectura','Primera Conversacion', 'Revisita', 'Curso Biblico', 'Discurso', 'Otro']
+opcion_seleccionada.set(opciones_asignaciones[0])
 OptionMenu(ventana, opcion_seleccionada, *opciones_asignaciones).pack()
-Label(ventana, text='').pack()
 
 #Entrada Nombre
 Label(ventana, text="Nombre:", font=fuente_opciones).pack()
 Entry(ventana, textvariable=nombre_entry).pack()
-Label(ventana, text='').pack()
 
 #Entrada Ayudante
-Label(ventana, text="Ayudante:", font=fuente_opciones).pack()
+Label(ventana, text='Ayudante:', font=fuente_opciones).pack()
 Entry(ventana, textvariable=ayudante_entry).pack()
-Label(ventana, text='').pack()
+
+#Entrada Aclaraciones
+Label(ventana, text='Aclaraciones: ', font=fuente_opciones).pack()
+Entry(ventana, textvariable=aclaraciones_entry).pack()
 
 #Opciones Salas
 opcion_principal = tk.BooleanVar()
 opcion_auxiliar = tk.BooleanVar()
-opcion_sala_principal = Checkbutton(ventana, text='Sala Principal', variable=opcion_principal, command= asignacion_sala, font=fuente_opciones)
-opcion_sala_principal.pack()
-opcion_sala_auxiliar = Checkbutton(ventana, text='Sala Auxiliar',variable=opcion_auxiliar, command=asignacion_sala, font=fuente_opciones)
-opcion_sala_auxiliar.pack()
-Label(ventana, text='').pack()
+opcion_sala_principal = Checkbutton(ventana, text='Sala Principal', variable=opcion_principal, command= asignacion_sala, font=fuente_opciones).pack()
+opcion_sala_auxiliar = Checkbutton(ventana, text='Sala Auxiliar',variable=opcion_auxiliar, command=asignacion_sala, font=fuente_opciones).pack()
 
 # Botón para generar la asignación
 Button(ventana, text="Generar Asignación", command=generar_asignacion).pack()
@@ -123,7 +104,6 @@ Button(ventana, text="Generar Asignación", command=generar_asignacion).pack()
 #Mensaje de Asiganacion Descargada
 mensaje_completado_nombre = Label(ventana, text='', fg='green')
 mensaje_completado_nombre.pack()
-Label(ventana, text='').pack()
 
 # Enlace del evento de presionar Enter a la función de generar asignación
 ventana.bind('<Return>', activar_generar_asignacion)
--- funciones.py
+++ funciones.py
@@ -1,50 +1,63 @@
+from tkinter import Tk, Label, Entry, Button, Checkbutton, StringVar, OptionMenu
+from PIL import Image, ImageDraw, ImageFont
+from tkcalendar import DateEntry
+import re
+from datetime import datetime, timedelta
+import tkinter as tk
 import os
+dibujo = None
 
-def nombre_mayuscula(a):
-    palabras = a.split()
-    nombre = palabras[0].capitalize()
-    apellido = palabras[-1].capitalize()
-    nombre_mayus = nombre + ' '.join(palabras[1:-1]) + " " + apellido
-    return (nombre_mayus)
+def guardar_imagen(imagen, texto_nombre):
 
-def guardar_imagen(imagen, nombre_archivo, texto_nombre):
+    # Reemplazar caracteres no válidos en el nombre de archivo
+    nombre_archivo = re.sub(r"[^\w\s-]", "", texto_nombre.strip())
+    nombre_archivo = re.sub(r"[\s]+", "_", nombre_archivo)
+
+    # Generar el nombre de archivo único basado en el texto
+    nombre_archivo = nombre_archivo + ".jpg"
     ruta_escritorio = os.path.expanduser('~/Desktop')
     carpeta_asignaciones = os.path.join(ruta_escritorio, 'Asignaciones')
     if not os.path.exists(carpeta_asignaciones):
         os.mkdir(carpeta_asignaciones)
     ruta_archivo = os.path.join(carpeta_asignaciones, nombre_archivo)
     imagen.save(ruta_archivo)
-    msj = f'Asignacion de {nombre_mayuscula(texto_nombre)} descargada'
+    msj = f'Asignacion de {texto_nombre} descargada'
     return(msj)
 
-def asignacion_actividad(texto_actividad, dibujo, texto_nombre, texto_ayudante, fuente):
+def asignacion_actividad(texto_actividad, dibujo, texto_nombre, texto_ayudante, fuente, aclaraciones_entry, fuente_aclaraciones):
+        
         ban = 0
         if texto_actividad == "lectura":
-            dibujo.rectangle([(53, 298), (65, 309)], fill="blue", width=2)
+            dibujo.rectangle([(53, 298), (65, 309)], fill="black", width=2)
             ban = 0
         elif texto_actividad == "primera conversacion":
-            dibujo.rectangle([(53, 320), (65, 331)], fill="blue", width=2)
+            dibujo.rectangle([(53, 320), (65, 331)], fill="black", width=2)
             ban = 1
         elif texto_actividad == "revisita":
-            dibujo.rectangle([(53, 365), (65, 376)], fill="blue", width=2)
+            dibujo.rectangle([(53, 365), (65, 376)], fill="#black", width=2)
+            dibujo.text((85,340), aclaraciones_entry, fill=(0, 0, 0), font=fuente_aclaraciones)
             ban = 1
         elif texto_actividad == "curso biblico":
-            dibujo.rectangle([(305, 298), (317, 309)], fill="blue", width=2)
+            dibujo.rectangle([(305, 298), (317, 309)], fill="black", width=2)
+            dibujo.text((85,340), aclaraciones_entry, fill=(0, 0, 0), font=fuente_aclaraciones)
             ban = 1 
         elif texto_actividad == "discurso":
-            dibujo.rectangle([(305, 320), (317, 331)], fill="blue", width=2)
+            dibujo.rectangle([(305, 320), (317, 331)], fill="black", width=2)
             ban = 0
         else:
-            dibujo.text([(305, 343),(317, 354)], fill='blue', width=2)
+            dibujo.text([(305, 343),(317, 354)], fill='black', width=2)
+            dibujo.text((85,340), aclaraciones_entry, fill=(0, 0, 0), font=fuente_aclaraciones)
             ban = 1
-        if ban == 0:
-            dibujo.text((136, 93), nombre_mayuscula(texto_nombre), fill=(0, 0, 0), font=fuente)
-        elif ban == 1:
-            dibujo.text((136, 93), nombre_mayuscula(texto_nombre), fill=(0, 0, 0), font=fuente)
-            dibujo.text((155, 142), nombre_mayuscula(texto_ayudante), fill=(0, 0, 0), font=fuente)
 
+        if ban == 0:
+            dibujo.text((136, 93), (texto_nombre), fill=(0, 0, 0), font=fuente)
+        elif ban == 1:
+            dibujo.text((136, 93), (texto_nombre), fill=(0, 0, 0), font=fuente)
+            dibujo.text((155, 142), (texto_ayudante), fill=(0, 0, 0), font=fuente)
+            
 def asignacion_sala(a,b,dibujo):
     if a.get():
-        dibujo.rectangle([(53, 450), (65, 461)], fill="blue", width=2)
+        dibujo.rectangle([(53, 450), (65, 461)], fill="black", width=2)
     elif b.get():
-        dibujo.rectangle([(53, 473),(65, 484)],fill="blue",width=2)
+        dibujo.rectangle([(53, 473),(65, 484)],fill="black",width=2)
+
