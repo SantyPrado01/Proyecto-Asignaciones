2b14974c494c78e5ceeb866afc71d9af82922861
SantyPrado01 <santiago01prado@gmail.com>
2023-09-01 17:10:43 -0300

    Version 1.1

--- funciones.py
+++ funciones.py
@@ -28,25 +28,25 @@ def asignacion_actividad(texto_actividad, dibujo, texto_nombre, texto_ayudante, 
         
         ban = 0
         if texto_actividad == "lectura":
-            dibujo.rectangle([(53, 298), (65, 309)], fill="black", width=2)
+            dibujo.rectangle([(53, 298), (65, 309)], fill="#946FBD", width=2)
             ban = 0
         elif texto_actividad == "primera conversacion":
-            dibujo.rectangle([(53, 320), (65, 331)], fill="black", width=2)
+            dibujo.rectangle([(53, 320), (65, 331)], fill="#946FBD", width=2)
+            dibujo.text((85,338), aclaraciones_entry, fill=(0, 0, 0), font=fuente_aclaraciones)
             ban = 1
         elif texto_actividad == "revisita":
-            dibujo.rectangle([(53, 365), (65, 376)], fill="#black", width=2)
-            dibujo.text((85,340), aclaraciones_entry, fill=(0, 0, 0), font=fuente_aclaraciones)
+            dibujo.rectangle([(53, 365), (65, 376)], fill="#946FBD", width=2)
+            dibujo.text((85,380), aclaraciones_entry, fill=(0, 0, 0), font=fuente_aclaraciones)
             ban = 1
         elif texto_actividad == "curso biblico":
-            dibujo.rectangle([(305, 298), (317, 309)], fill="black", width=2)
-            dibujo.text((85,340), aclaraciones_entry, fill=(0, 0, 0), font=fuente_aclaraciones)
+            dibujo.rectangle([(305, 298), (317, 309)], fill="#946FBD", width=2)
             ban = 1 
         elif texto_actividad == "discurso":
-            dibujo.rectangle([(305, 320), (317, 331)], fill="black", width=2)
+            dibujo.rectangle([(305, 320), (317, 331)], fill="#946FBD", width=2)
             ban = 0
         else:
-            dibujo.text([(305, 343),(317, 354)], fill='black', width=2)
-            dibujo.text((85,340), aclaraciones_entry, fill=(0, 0, 0), font=fuente_aclaraciones)
+            dibujo.rectangle([(305, 343),(317, 354)], fill='#946FBD', width=2)
+            dibujo.text((383,338), aclaraciones_entry, fill=(0, 0, 0), font=fuente_aclaraciones)
             ban = 1
 
         if ban == 0:
@@ -57,7 +57,7 @@ def asignacion_actividad(texto_actividad, dibujo, texto_nombre, texto_ayudante, 
             
 def asignacion_sala(a,b,dibujo):
     if a.get():
-        dibujo.rectangle([(53, 450), (65, 461)], fill="black", width=2)
+        dibujo.rectangle([(53, 450), (65, 461)], fill="#946FBD", width=2)
     elif b.get():
-        dibujo.rectangle([(53, 473),(65, 484)],fill="black",width=2)
+        dibujo.rectangle([(53, 473),(65, 484)],fill="#946FBD",width=2)
 
