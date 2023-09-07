c1a8a9a50dfd056d11908562eac0ab9fce32966b
SantyPrado01 <santiago01prado@gmail.com>
2023-09-06 23:38:46 -0300

    Agregado de Base de Datos

--- funciones.py
+++ funciones.py
@@ -1,10 +1,18 @@
-from tkinter import Tk, Label, Entry, Button, Checkbutton, StringVar, OptionMenu
+from tkinter import messagebox, Button
 from PIL import Image, ImageDraw, ImageFont
 from tkcalendar import DateEntry
 import re
 from datetime import datetime, timedelta
 import tkinter as tk
+from tkinter import ttk
 import os
+from comandosSql import *
+import sqlite3
+from reportlab.lib.pagesizes import letter
+from reportlab.lib import pdfencrypt
+from reportlab.pdfgen import canvas
+
+
 dibujo = None
 
 def guardar_imagen(imagen, texto_nombre):
@@ -27,21 +35,21 @@ def guardar_imagen(imagen, texto_nombre):
 def asignacion_actividad(texto_actividad, dibujo, texto_nombre, texto_ayudante, fuente, aclaraciones_entry, fuente_aclaraciones):
         
         ban = 0
-        if texto_actividad == "lectura":
+        if texto_actividad == "Lectura":
             dibujo.rectangle([(53, 298), (65, 309)], fill="#946FBD", width=2)
             ban = 0
-        elif texto_actividad == "primera conversacion":
+        elif texto_actividad == "Primera Conversacion":
             dibujo.rectangle([(53, 320), (65, 331)], fill="#946FBD", width=2)
             dibujo.text((85,338), aclaraciones_entry, fill=(0, 0, 0), font=fuente_aclaraciones)
             ban = 1
-        elif texto_actividad == "revisita":
+        elif texto_actividad == "Revisita":
             dibujo.rectangle([(53, 365), (65, 376)], fill="#946FBD", width=2)
             dibujo.text((85,380), aclaraciones_entry, fill=(0, 0, 0), font=fuente_aclaraciones)
             ban = 1
-        elif texto_actividad == "curso biblico":
+        elif texto_actividad == "Curso Biblico":
             dibujo.rectangle([(305, 298), (317, 309)], fill="#946FBD", width=2)
             ban = 1 
-        elif texto_actividad == "discurso":
+        elif texto_actividad == "Discurso":
             dibujo.rectangle([(305, 320), (317, 331)], fill="#946FBD", width=2)
             ban = 0
         else:
@@ -55,9 +63,197 @@ def asignacion_actividad(texto_actividad, dibujo, texto_nombre, texto_ayudante, 
             dibujo.text((136, 93), (texto_nombre), fill=(0, 0, 0), font=fuente)
             dibujo.text((155, 142), (texto_ayudante), fill=(0, 0, 0), font=fuente)
             
-def asignacion_sala(a,b,dibujo):
-    if a.get():
+def asignacion_sala(sala_a,sala_b,dibujo):
+    if sala_a:
         dibujo.rectangle([(53, 450), (65, 461)], fill="#946FBD", width=2)
-    elif b.get():
+    elif sala_b:
         dibujo.rectangle([(53, 473),(65, 484)],fill="#946FBD",width=2)
+
+def insertar_asignacion(asignacion):
+    base_datos = sqlite3.connect('hermanos.bd')
+    cursor = base_datos.cursor()
+    cursor.execute("INSERT OR IGNORE INTO asignacion (nombre) VALUES (?)", (asignacion,))
+    base_datos.commit()
+
+def insertar_fecha(fecha):
+    base_datos = sqlite3.connect('hermanos.bd')
+    cursor = base_datos.cursor()
+    cursor.execute("INSERT OR IGNORE INTO fecha (fecha) VALUES (?)", (fecha,))
+    base_datos.commit()
+
+def insertar_sala (sala):
+    base_datos = sqlite3.connect('hermanos.bd')
+    cursor = base_datos.cursor()
+    cursor.execute("INSERT OR IGNORE INTO salas (nombre) VALUES (?)", (sala,))
+    base_datos.commit()
+
+def insertar_ayudante(ayudante):
+    base_datos = sqlite3.connect('hermanos.bd')
+    cursor = base_datos.cursor()
+    cursor.execute("INSERT OR IGNORE INTO ayudante (nombre) VALUES (?)", (ayudante,))
+    base_datos.commit()
+
+def guardar_asignacion(nombre, ayudante, asignacion, fecha, sala):
+    
+    insertar_asignacion(asignacion)
+    insertar_ayudante(ayudante)
+    insertar_fecha(fecha)
+    insertar_sala(sala)
+
+    base_datos = sqlite3.connect('hermanos.bd')
+    cursor = base_datos.cursor()
+    # Obtiene los IDs de marca y categoría
+    cursor.execute("SELECT ayudante_id FROM ayudante WHERE nombre = ?", (ayudante,))
+    ayudante_id = cursor.fetchone()
+
+    cursor.execute("SELECT asignacion_id FROM asignacion WHERE nombre = ?", (asignacion,))
+    asignacion_id = cursor.fetchone()
+
+    cursor.execute("SELECT fecha_id FROM fecha WHERE fecha = ?", (fecha,))
+    fecha_id = cursor.fetchone()
+
+    cursor.execute("SELECT sala_id FROM salas WHERE nombre = ?", (sala,))
+    sala_id = cursor.fetchone()
+    
+    cursor.execute("INSERT INTO hermanos (nombre, ayudante_id, asignacion_id, fecha_id, sala_id) VALUES (?, ?, ?, ?, ?)",
+                   (nombre, ayudante_id[0], asignacion_id[0], fecha_id[0], sala_id[0]))
+
+    base_datos.commit()      
+
+base_datos.close()
+
+def descargar_pdf(treeview, filename, nombre_a_indice):
+
+    ruta_escritorio = os.path.expanduser('~/Desktop')
+    carpeta_asignaciones = os.path.join(ruta_escritorio, 'Asignaciones')
+    if not os.path.exists(carpeta_asignaciones):
+        os.mkdir(carpeta_asignaciones)
+    ruta_archivo = os.path.join(carpeta_asignaciones, filename)
+
+    archivo = canvas.Canvas(ruta_archivo, pagesize=letter)
+
+    # Configurar los márgenes, fuentes y colores
+    left_margin = 10
+    right_margin = 10
+    top_margin = 35
+    bottom_margin = 10
+
+    width, height = letter
+    archivo.setPageSize((width, height))
+    archivo.setFont("Helvetica", 12)
+
+    # Configurar el encabezado del PDF
+    columnas = list(nombre_a_indice.keys())
+    x = left_margin
+    y = height - top_margin
+    espacio_entre_lineas = 15  # Espacio vertical entre líneas
+
+    for col in columnas:
+        encabezado = treeview.heading(col, "text")
+        x_centered = x + (100 - archivo.stringWidth(encabezado, "Helvetica", 12)) / 2
+        archivo.drawString(x_centered, y, encabezado)
+        x += 125  # Espacio horizontal entre columnas
+
+    # Obtener los datos del Treeview
+    filas = treeview.get_children()
+    y -= espacio_entre_lineas  # Mover hacia abajo para los datos
+
+    for fila in filas:
+        x = left_margin
+        datos = [treeview.item(fila, 'values')[nombre_a_indice[col]] for col in columnas]
+
+        # Obtener el estilo de fondo de la fila
+        fecha_estilo = fecha_color(datos[3])  # Suponemos que la fecha está en la quinta columna
+
+        for i, dato in enumerate(datos, start=1):
+            # Configurar el color de fondo según la fecha
+            if fecha_estilo == "FechaVerde":
+                archivo.setFillColorRGB(160 / 255, 255 / 255, 143 / 255)  # Verde
+            elif fecha_estilo == "FechaRoja":
+                archivo.setFillColorRGB(255 / 255, 143 / 255, 146 / 255)  # Rojo
+            else:
+                archivo.setFillColorRGB(1, 1, 1)  # Blanco (predeterminado)
+
+            width = 100  # Ancho de la celda
+            height = espacio_entre_lineas  # Altura de la celda
+            text_width = archivo.stringWidth(str(dato), "Helvetica", 12)
+            x_centered = x + (width - text_width) / 2
+
+            archivo.rect(x, y, width, -height, fill=True, stroke=False)
+            archivo.setFillColorRGB(0, 0, 0)  # Color de texto negro
+            archivo.drawString(x_centered, y - 10, str(dato))
+            x += 125  # Espacio horizontal entre columnas
+
+        y -= espacio_entre_lineas  # Mover hacia abajo para la siguiente fila
+    archivo.showPage()  # Mostrar la página actual
+    archivo.save()
+
+def fecha_color(fecha_str):
+    fecha = datetime.strptime(fecha_str, "%d-%m-%Y")
+    dos_meses_atras = datetime.now() - timedelta(days=60)
+    
+    if fecha < dos_meses_atras:
+        return "FechaVerde"
+    else:
+        return "FechaRoja"
+
+def buscar_todos():
+    base_datos = sqlite3.connect('hermanos.bd')
+    cursor = base_datos.cursor()
+    
+    cursor.execute('SELECT * FROM hermanos')
+    hermanos = cursor.fetchall()
+    
+    if not hermanos:
+        messagebox.showerror('Error', f'No Encontrado')
+    else:
+        ventana_emergente_2 = tk.Toplevel()
+        tree = ttk.Treeview(ventana_emergente_2, columns=('Nombre', 'Ayudante', 'Asignacion', 'Fecha', 'Sala'))
+        
+        tree.column("#0", width =0, stretch=tk.NO)
+        tree.heading("#1", text="Nombre", anchor=tk.CENTER)
+        tree.heading("#2", text="Ayudante", anchor=tk.CENTER)
+        tree.heading("#3", text="Asignacion", anchor=tk.CENTER)
+        tree.heading("#4", text="Fecha", anchor=tk.CENTER)
+        tree.heading("#5", text="Sala", anchor=tk.CENTER)
+        
+        for i in range(1, 6):
+            tree.column(f"#{i}", anchor=tk.CENTER)
+        
+        style = ttk.Style()
+        style.configure("FechaVerde.Treeview", background="green")
+        style.configure("FechaRoja.Treeview", background="red")
+        
+        for tema in hermanos:
+            id, nombre, ayudante_id, asignacion_id, fecha_id, sala_id = tema
+            
+            cursor.execute("SELECT nombre FROM ayudante WHERE ayudante_id=?", (ayudante_id,))
+            ayudante_nombre = cursor.fetchone()[0]
+            
+            cursor.execute("SELECT nombre FROM asignacion WHERE asignacion_id=?", (asignacion_id,))
+            asignacion_nombre = cursor.fetchone()[0]
+            
+            cursor.execute("SELECT fecha FROM fecha WHERE fecha_id=?", (fecha_id,))
+            fecha_dato = cursor.fetchone()[0]
+            
+            cursor.execute("SELECT nombre FROM salas WHERE sala_id=?", (sala_id,))
+            sala_nombre = cursor.fetchone()[0]
+            
+            fecha_estilo = fecha_color(fecha_dato)
+            
+            tree.insert('', 'end', values=[nombre, ayudante_nombre, asignacion_nombre, fecha_dato, sala_nombre], tags=(fecha_estilo,))
+        
+        tree.tag_configure("FechaVerde", background="#A0FF8F")
+        tree.tag_configure("FechaRoja", background="#FF8F92")
+
+        tree.grid(row=0, column=0, padx=10, pady=10)
+        nombre_pdf = 'Asignaciones.pdf'
+
+        nombre_a_indice = {'#1':0,'#2': 1, '#3': 2, '#4': 3, '#5': 4}
+        # Para cada columna, elimina el '#' si está presente
+        boton_descargar_pdf = Button(ventana_emergente_2, text='Descargar PDF', command=lambda:descargar_pdf(tree, nombre_pdf, nombre_a_indice))
+        boton_descargar_pdf.grid(row=1, column=0)
+    
+    base_datos.close()
+
 
--- menu.py
+++ menu.py
@@ -6,16 +6,29 @@ from datetime import datetime, timedelta
 import tkinter as tk
 from funciones import *
 import locale
+from comandosSql import * 
+
 locale.setlocale(locale.LC_TIME, "es_ES.utf8")
 dibujo = None
 
-
 def generar_asignacion():
+
     global ingresar_semana_date, nombre_entry, ayudante_entry, dibujo, aclaraciones_entry
+
     try:
-        texto_actividad = opcion_seleccionada.get().lower()
-        texto_nombre = nombre_entry.get()
-        texto_ayudante = ayudante_entry.get()
+        nombre = nombre_entry.get()
+        ayudante = ayudante_entry.get()
+        asignacion = opcion_seleccionada.get()
+        fecha = ingresar_semana_date.get()
+        sala_a = opcion_principal.get()
+        sala_b = opcion_auxiliar.get()
+
+        sala = None
+        if sala_a:
+            sala = 'Sala A'
+        elif sala_b:
+            sala = 'Sala B'
+
         aclaraciones = aclaraciones_entry.get()
 
         # Cargar la imagen original en cada iteración
@@ -25,19 +38,21 @@ def generar_asignacion():
         fuente = ImageFont.truetype("fonts/Calibri.ttf", 26)
         fuente_aclaraciones = ImageFont.truetype("fonts/Calibri.ttf", 18)
         # Agregar Semana a la Imagen 
-        dibujo.text((112,190), ingresar_semana_date.get(), fill=(0, 0, 0), font=fuente)
+        dibujo.text((112,190), fecha, fill=(0, 0, 0), font=fuente)
 
         #Definimos Actividad
-        asignacion_actividad(texto_actividad,dibujo, texto_nombre, texto_ayudante, fuente, aclaraciones, fuente_aclaraciones)
+        asignacion_actividad(asignacion, dibujo, nombre, ayudante, fuente, aclaraciones, fuente_aclaraciones)
 
         #Definimos Sala
-        asignacion_sala(opcion_principal,opcion_auxiliar,dibujo)
+        asignacion_sala(sala_a, sala_b, dibujo)
 
         #Guardamos Imagen
-        guardar_imagen(imagen, texto_nombre)
-        
+        guardar_imagen(imagen, nombre)
+
+        guardar_asignacion(nombre, ayudante, asignacion, fecha, sala)
+
         #Mensaje de Asignacion descargada
-        mensaje_completado_nombre.config(text=guardar_imagen(imagen, texto_nombre))
+        mensaje_completado_nombre.config(text=guardar_imagen(imagen, nombre))
 
         #Variables Vacias 
         nombre_entry.set("")
@@ -95,11 +110,15 @@ Entry(ventana, textvariable=aclaraciones_entry).pack()
 #Opciones Salas
 opcion_principal = tk.BooleanVar()
 opcion_auxiliar = tk.BooleanVar()
-opcion_sala_principal = Checkbutton(ventana, text='Sala Principal', variable=opcion_principal, command= asignacion_sala, font=fuente_opciones).pack()
-opcion_sala_auxiliar = Checkbutton(ventana, text='Sala Auxiliar',variable=opcion_auxiliar, command=asignacion_sala, font=fuente_opciones).pack()
+opcion_sala_principal = Checkbutton(ventana, text='Sala Principal', variable=opcion_principal, font=fuente_opciones).pack()
+opcion_sala_auxiliar = Checkbutton(ventana, text='Sala Auxiliar',variable=opcion_auxiliar, font=fuente_opciones).pack()
 
 # Botón para generar la asignación
-Button(ventana, text="Generar Asignación", command=generar_asignacion).pack()
+boton_guardar = Button(ventana, text="Guardar Asignación", command=generar_asignacion)
+boton_guardar.pack()
+
+boton_mostar = Button(ventana, text='Buscar Asignaciones', command=buscar_todos)
+boton_mostar.pack()
 
 #Mensaje de Asiganacion Descargada
 mensaje_completado_nombre = Label(ventana, text='', fg='green')
